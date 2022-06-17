7fd92a68038a178b3403a4e763f4ead6e84c1913
Artsiom Martynau <xzfantom@gmail.com>
2022-06-17 15:31:24 +0400

    fix: Правки по код-ревью

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
+const errorMessage = 'Некорректный ввод!';
+
 function task1() {
-  let number = parseInt(window.prompt('Введите число'), 10);
-  let base = parseInt(window.prompt('Введите основание'), 10);
-  if (number === NaN || base === NaN) {
-    console.log('Некорректный ввод!');
+  let number = parseInt(prompt('Введите число'), 10);
+  let base = parseInt(prompt('Введите основание'), 10);
+  if (Object.is(number, NaN) || Object.is(base, NaN)) {
+    console.log(errorMessage);
   } else {
     console.log(number.toString(base));
   }
 }
 
 function task2() {
-  let firstNumber = parseInt(window.prompt('Введите число'), 10);
-  if (firstNumber === NaN) {
-    console.log('Некорректный ввод!');
+  let firstNumber = parseInt(prompt('Введите число'), 10);
+  if (Object.is(firstNumber, NaN)) {
+    console.log(errorMessage);
 
     return;
   }
-  let secondNumber = parseInt(window.prompt('Введите число'), 10);
-  if (secondNumber === NaN) {
-    console.log('Некорректный ввод!');
+  let secondNumber = parseInt(prompt('Введите число'), 10);
+  if (Object.is(secondNumber, NaN)) {
+    console.log(errorMessage);
 
     return;
   }
   console.log(`Ответ: ${firstNumber + secondNumber}, ${firstNumber / secondNumber}`);
 }
+
+document.getElementById('task1').addEventListener('click', task1);
+document.getElementById('task2').addEventListener('click', task2);
